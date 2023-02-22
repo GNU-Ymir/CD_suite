067aebf9a4f01f7f52adf64bddd01a4e224bb041
Emile Cadorel <ecadorel@gmail.com>
2023-02-22 13:19:52 +0100

    update of gymir and gyllir compilation

--- amd64/deb/gymir_update.sh
+++ amd64/deb/gymir_update.sh
@@ -5,41 +5,13 @@ INSTALL_DIR=$2
 echo $GCC_VERSION
 echo $GCC_MAJOR_VERSION
 
-mkdir -p ${INSTALL_DIR}
 
-sudo apt-get -y update
-sudo apt-get install -y --no-install-recommends sudo pkg-config git build-essential software-properties-common aspcud unzip curl wget
-sudo apt-get install -y --no-install-recommends gcc g++ flex autoconf automake libtool cmake emacs patchelf
-sudo apt-get install -y --no-install-recommends gcc-multilib g++-multilib libgc-dev libgmp-dev libbfd-dev zlib1g-dev
-sudo apt-get install -y build-essential
-
-
-mkdir ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/
-cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/
-mkdir ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-src
-
-cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/
-git clone --depth=1 git://gcc.gnu.org/git/gcc.git gcc-src
-cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-src
-git fetch --tags --depth=1
-git checkout releases/gcc-${GCC_VERSION}
-
-cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-src/gcc/
-git clone --depth=1 https://github.com/GNU-Ymir/gymir.git ymir
-
-
-cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-src/
-
-rm -rf ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-build
-mkdir ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-build
-
-./contrib/download_prerequisites
-
-cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-build/
-../gcc-src/configure --enable-languages=c,d,ymir --with-gcc-major-version-only --program-suffix=-${GCC_MAJOR_VERSION} --prefix=/usr --program-prefix=x86_64-linux-gnu- --libexecdir=/usr/lib --libdir=/usr/lib --with-sysroot=/ --with-arch-directory=amd64 --enable-multiarch --with-arch-32=i686 --with-abi=m64 --with-multilib-list=m32,m64,mx32 --enable-multilib --enable-checking=release --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --disable-bootstrap
 
 rm -rf ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-install
 mkdir ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-install
+
+cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-src/gcc/ymir/
+git pull origin master
 
 cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-build/
 rm gcc/ymir/*.o
@@ -79,6 +51,7 @@ mv ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gcc-deb.deb ${INSTALL_DIR}/gyc-${GCC_
 sudo dpkg -i ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/gyc_${GCC_VERSION}_amd64.deb
 cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/
 
+rm -rf ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/midgard/
 git clone https://github.com/GNU-Ymir/yruntime.git midgard
 cd ${INSTALL_DIR}/gyc-${GCC_VERSION}-build/midgard/
 mkdir .build
