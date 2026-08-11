ec89ab9b2dd2ed0a6831ec443b352b7bad6fc4b8
Emile Cadorel <ecadorel@gmail.com>
2026-08-11 22:13:26 +0200

    feat: add gyllir building version, to complete chain

--- amd64/deb/utils/versions.py
+++ amd64/deb/utils/versions.py
@@ -122,8 +122,8 @@ STAGES: dict[str, CxxStage | BootstrapStage] = {
         # target gcc-15, compiled with gcc-15, both on ubuntu 26.04
         prev_gyc="15_1.1.1",
         prev_gyllir="1.1.0",
-        versions=GycVersions (compiler="15", target="15", ymir="1.2.0", bootstrap="1.2.0", midgard="1.2.1"),
+        versions=GycVersions (compiler="15", target="15", ymir="1.2.2", bootstrap="1.2.2", midgard="1.2.1"),
         ubuntu_version=UBUNTU_FOR_GCC15,
-        gyllir=None,
+        gyllir=GyllirSpec (gyc="15_1.2.2", compile_with="15", gyllir_version="1.3.0", ubuntu_version=UBUNTU_FOR_GCC15, prev_gyllir="1.2.0"),
     ),
 }
